36f01f081a78210d7ac81caa238cb5f3654a206c
dacods <dacods.21@gmail.com>
2024-03-04 18:41:04 +0000

    Switched == to != and recompiled

--- _printf.c
+++ _printf.c
@@ -25,7 +25,9 @@ int _printf(const char *format, ...)
 		if (*format == '%' && get_func(1 + format) != NULL)
 		{
 			format++;
-			if (*format == 's')
+			if (*format == 'd' || *format == 'i')
+				str += printf_integers(format, args);
+			else if (*format == 's')
 				str += printf_string(format, args);
 			else if (*format == 'c')
 				str += printf_char(format, args);
